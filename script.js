d87a5eb74106e1e1df29251fc6f5bfcfc5ba2428
GhostlyNix <nixtrafalaria124@gmail.com>
2023-05-30 19:55:26 -0300

    update script

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function showAlert() { alert('Hola, esta es una alerta desde JavaScript :]');}
 
+if (form) {
+
 const form = document.getElementById('myForm');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     validateForm();
-});
+  });
 
 function validateEmail(email) {
     const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
   return regex.test(email)
-}
+  }
 
 function validateForm() {
     const emailInput = document.getElementById('email');
@@ -20,6 +22,7 @@ function validateForm() {
         alert('Por favor ingrese un correo eletrónico válido');}
     else {
       alert('Correo electrónico enviado correctamente');}
+  }
 }
 
 document.querySelector("button.button-menu-toggle")
